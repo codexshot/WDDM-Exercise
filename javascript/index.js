4e877db44a6953d819f41ee5f5e579cd5b44aeb2
Himanshu <himanshu.tempospace@gmail.com>
2024-01-25 22:10:06 -0500

    generating random numbers between 1-100

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,34 @@
 var button = document.getElementById("pause");
 
 button.onclick = function () {
-  if (document.getElementById("likes").style.display == "none") {
-    document.getElementById("likes").style.display = "block";
-    document.getElementById("pause").innerHTML = "Generate Random Number";
-  } else {
-    document.getElementById("likes").style.display = "none";
-    document.getElementById("pause").innerHTML = "Pause";
-  }
+  changeText();
+  // if (document.getElementById("likes").style.display == "none") {
+  //   document.getElementById("likes").style.display = "block";
+  //   document.getElementById("pause").innerHTML = "Generate Random Number";
+ 
+  // } else {
+  //   document.getElementById("likes").style.display = "none";
+  //   document.getElementById("pause").innerHTML = "Generate again";
+
+  // }
 };
+// Generate a random number between 1 and 100
+function getRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+  }
+
+
+
+function changeText() {
+    // Get the element by its id
+    var element = document.getElementById("randomText");
+
+    // Check if the element exists
+    if (element) {
+      // Change the text content
+    var  text=getRandomNumber();
+      element.textContent = `Your lucky number is ${text}`;
+    } else {
+      console.error("Element not found");
+    }
+  }
